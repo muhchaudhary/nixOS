5fe4cb1542125f668006f292e0dbc9ad112d5500
muhchaudhary <muhammadahmchaudhary@gmail.com>
2023-12-05 14:27:12 -0500

    working vscode home-manager setup with nvidia fixes applies

--- programs/vscode-nvfix.nix
+++ programs/vscode-nvfix.nix
@@ -29,32 +29,33 @@
             --add-needed ${libglvnd}/lib/libEGL.so.1\
             $out/lib/vscode/code
         '';
+        version = "latest";
       });
+    extensions = with pkgs;
+    with vscode-extensions;
+      [
+        kamadorueda.alejandra
+        bbenoist.nix
+        esbenp.prettier-vscode
+        ms-python.python
+        ms-python.vscode-pylance
+        timonwong.shellcheck
+        foxundermoon.shell-format
+        eamodio.gitlens
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "popping-and-locking-vscode-black";
+          publisher = "philsinatra";
+          version = "1.1.7";
+          sha256 = "sha256-D05Bqoahrd+r57/GhbDbBeaWmZ4vTByK+P8UmyYNVR0=";
+        }
+        {
+          name = "showtime";
+          publisher = "AlexFromXD";
+          version = "0.0.3";
+          sha256 = "sha256-MJ24fs0qzdWLi6anaI60AufwzlQ2Sx5NFsHb6NJ/wX8=";
+        }
+      ];
   };
-  extensions = with pkgs;
-  with vscode-extensions;
-    [
-      kamadorueda.alejandra
-      bbenoist.nix
-      esbenp.prettier-vscode
-      ms-python.python
-      ms-python.vscode-pylance
-      timonwong.shellcheck
-      foxundermoon.shell-format
-      eamodio.gitlens
-    ]
-    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "popping-and-locking-vscode-black";
-        publisher = "philsinatra";
-        version = "1.1.7";
-        sha256 = "1234";
-      }
-      {
-        name = "showtime";
-        publisher = "AlexFromXD";
-        version = "0.0.3";
-        sha256 = "1234";
-      }
-    ];
 }
