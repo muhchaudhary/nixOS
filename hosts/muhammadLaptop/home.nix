b188b4aa4165c65f5491238f9fcfbccb30ee8fbe
muhammadahmchaudhary@gmail.com <muhchaudhary>
2023-12-23 16:12:59 -0500

    run update

--- hosts/muhammadLaptop/home.nix
+++ hosts/muhammadLaptop/home.nix
@@ -13,8 +13,9 @@
     .overrideAttrs
     (prevAttrs: {
       src = builtins.fetchTarball {
-        url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
-        sha256 = "15n680mydgjfxz6cvg8c5057yzrwzx86y4aq1764kwhbzff23420";
+        # run curl -I https://update.code.visualstudio.com/latest/linux-x64/insider to get latest url
+        url = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/9621add46007f7a1ab37d1fce9bcdcecca62aeb0/code-insider-x64-1703050604.tar.gz";
+        sha256 = "017630xgr64qjva73imb56fcqr858xfcsbdgq97akawlxf1ydm5a";
       };
       preFixup = ''
         gappsWrapperArgs+=(
