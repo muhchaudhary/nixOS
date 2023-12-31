50eb8b72586a2b11dcb108497b17a2832e89baa6
muhchaudhary <muhammadahmchaudhary@gmail.com>
2023-12-31 13:10:27 -0500

    fix ags

--- wms/hyprland/programs/ags.nix
+++ wms/hyprland/programs/ags.nix
@@ -10,10 +10,7 @@
 
   programs.ags = with pkgs; {
     enable = true;
-<<<<<<< HEAD
     # not including config due to active development
-=======
->>>>>>> bbf3da2 (fix nvidia docker)
     #configDir = pkgs.aylurs-dots;
     extraPackages = [pkgs.libsoup_3];
   };
