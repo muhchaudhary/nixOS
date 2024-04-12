b041f5e867b14b94482431d36ce44ec294438e36
muhchaudhary <61593188+muhchaudhary@users.noreply.github.com>
2024-04-12 16:17:11 -0400

    add config for hyprspace

--- wms/hyprland/plugins/hyprspace.nix
+++ wms/hyprland/plugins/hyprspace.nix
@@ -8,5 +8,14 @@
     plugins = [
       inputs.hyprspace.packages.${pkgs.system}.Hyprspace
     ];
+    settings = {
+      plugin.overview = {
+        hideBackgroundLayers = true;
+        panelHeight = 150;
+        workspaceBorderSize = 10;
+        workspaceActiveBorder = "rgb(88c0d0)";
+        panelColor = "rgba(00000000)";
+      };
+    };
   };
 }
