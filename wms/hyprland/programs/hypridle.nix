fc89628fa9c1548f49e08dd58b529659dac9e867
muhchaudhary <61593188+muhchaudhary@users.noreply.github.com>
2024-04-13 15:03:56 -0400

    update hypridle/hyprlock config

--- wms/hyprland/programs/hypridle.nix
+++ wms/hyprland/programs/hypridle.nix
@@ -8,18 +8,23 @@
     inputs.hypridle.homeManagerModules.default
   ];
 
-  services.hypridle = with pkgs; {
+  services.hypridle = let
+    hyprlock = "${pkgs.hyprlock}/bin/hyprlock";
+    hyprctl = "${pkgs.hyprland}/bin/hyprctl";
+    loginctl = "${pkgs.systemd}/bin/loginctl";
+  in {
     enable = true;
-    beforeSleepCmd = "${systemd}/bin/loginctl lock-session";
-    lockCmd = lib.getExe config.programs.hyprlock.package;
+    lockCmd = "pidof hyprlock || ${hyprlock}";
+    beforeSleepCmd = "${hyprctl} dispatch dpms off";
+    afterSleepCmd = "${hyprctl} dispatch dpms on && ${loginctl} lock-session ";
     listeners = [
       {
         timeout = 200;
-        onTimeout = lib.getExe config.programs.hyprlock.package;
+        onTimeout = "${loginctl} lock-session";
       }
       {
         timeout = 330;
-        onTimeout = "${systemd}/bin/systemctl suspend";
+        onTimeout = "${pkgs.systemd}/bin/systemctl suspend";
       }
     ];
   };
