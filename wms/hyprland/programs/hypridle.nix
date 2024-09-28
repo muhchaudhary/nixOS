f2800889d465eb4e3a6b57bbacbf626f21911024
muhchaudhary <61593188+muhchaudhary@users.noreply.github.com>
2024-09-28 16:40:05 -0400

    fix hypridle config

--- wms/hyprland/programs/hypridle.nix
+++ wms/hyprland/programs/hypridle.nix
@@ -8,22 +8,28 @@
     hyprlock = "${pkgs.hyprlock}/bin/hyprlock";
     hyprctl = "${pkgs.hyprland}/bin/hyprctl";
     loginctl = "${pkgs.systemd}/bin/loginctl";
+    pidof = "${pkgs.procps}/bin/pidof";
+    notify-send = "${pkgs.libnotify}/bin/notify-send";
   in {
     enable = true;
     settings = {
       general = {
+        lock_cmd = "${pidof} ${hyprlock} || ${hyprlock}";
         before_sleep_cmd = "${loginctl} lock-session";
         after_sleep_cmd = "${hyprctl} dispatch dpms on && ${loginctl} lock-session ";
-        ignore_dbus_inhibit = true;
-        lock_cmd = "pidof ${hyprlock} || ${hyprlock}";
       };
-      listeners = [
+      listener = [
         {
           timeout = 200;
           on-timeout = "${loginctl} lock-session";
         }
         {
-          timeout = 330;
+          timeout = 300;
+          on-timeout = "${hyprctl} dispatch dpms off";
+          on-resume = "${hyprctl} dispatch dpms on";
+        }
+        {
+          timeout = 500;
           on-timeout = "${pkgs.systemd}/bin/systemctl suspend";
         }
       ];
